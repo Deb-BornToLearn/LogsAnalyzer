c4ef735622ad6bb128c137db8e915e5f67c89412
JABE <jabestrada@yahoo.com>
2020-02-19 13:49:09 +0800

    Initial implementation of StringChunker

---
+++ LogsAnalyzer.Renderers.WinForms/StringChunker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogsAnalyzer.Renderers.WinForms {
+    public class StringChunker {
+        public static List<ChunkDefinition> ComputeChunks(string input, int charsPerChunk) {
+            var chunkPositions = new List<ChunkDefinition>();
+            var inputLength = input.Length;
+
+            if (inputLength <= charsPerChunk) {
+                chunkPositions.Add(new ChunkDefinition(0, inputLength));
+                return chunkPositions;
+            }
+
+            
+            int currentStartPos = 0;
+            while (true) {
+                int chunkLength = charsPerChunk;
+                int currentPos = currentStartPos + chunkLength;
+                if (currentPos >= inputLength) {
+                    while (currentPos > inputLength) {
+                        chunkLength--;
+                        currentPos--;
+                    }
+                    chunkPositions.Add(new ChunkDefinition {
+                        StartPosition = currentStartPos,
+                        ChunkLength = chunkLength
+                    });
+                    break;
+                } else {
+                    while (currentPos < inputLength && input[currentPos] != ' ') {
+                        currentPos++;
+                        chunkLength++;
+                    }
+                    chunkPositions.Add(new ChunkDefinition {
+                        StartPosition = currentStartPos,
+                        ChunkLength = chunkLength
+                    });
+                    currentStartPos += chunkLength + 1;
+                    if (currentStartPos >= inputLength) break;
+                }
+            }
+            return chunkPositions;
+        }
+    }
+
+    public struct ChunkDefinition {
+        public int StartPosition;
+        public int ChunkLength;
+        public ChunkDefinition(int startPosition, int chunkLength) {
+            StartPosition = startPosition;
+            ChunkLength = chunkLength;
+        }
+    }
+}
