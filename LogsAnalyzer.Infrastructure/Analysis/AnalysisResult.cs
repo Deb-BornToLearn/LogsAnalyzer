cc14a976bc5d6352b3b19f069227ead089cf4b38
JABE <jabestrada@yahoo.com>
2020-03-10 22:52:25 +0800

    Removed redundant Text property

--- LogsAnalyzer.Infrastructure/Analysis/AnalysisResult.cs
+++ LogsAnalyzer.Infrastructure/Analysis/AnalysisResult.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace LogsAnalyzer.Infrastructure.Analysis {
     public class AnalysisResult : BaseAnalysisResult {
-        public string Text;
 
         public override string ToString() {
             return Text;
